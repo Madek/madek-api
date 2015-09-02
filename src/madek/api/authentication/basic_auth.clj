082f61a685dcd15b82b1718904aa211ccbc57c7c
Thomas Schank <DrTom@schank.ch>
2015-09-02 11:27:48 +0200

    The basic-auth extractor should not log warnings if there isn't any basic auth header

--- src/madek/api/authentication/basic_auth.clj
+++ src/madek/api/authentication/basic_auth.clj
@@ -1,5 +1,5 @@
 (ns madek.api.authentication.basic-auth
-  (:require 
+  (:require
     [clojure.data.codec.base64 :as base64]
     [clojure.tools.logging :as logging]
     [drtom.logbug.thrown :as thrown]
@@ -11,14 +11,10 @@
 
 (defn extract [request]
   (logging/debug 'extract request)
-  (try (let [auth-header (-> request :headers :authorization)
-             decoded-val (decode-base64 (last (re-find #"^Basic (.*)$" auth-header)))
-             [username password] (clojure.string/split (str decoded-val) #":" 2)]
-         (logging/debug 'auth-header auth-header)
-         (logging/debug 'decoded-val decoded-val)
-         (logging/debug 'up [username password])
-         {:username username :password password})
+  (try (when-let [auth-header (-> request :headers :authorization)]
+         (let [decoded-val (decode-base64 (last (re-find #"^Basic (.*)$" auth-header)))
+               [username password] (clojure.string/split (str decoded-val) #":" 2)]
+           {:username username :password password}))
        (catch Exception _
          (logging/warn "failed to extract basic-auth properties" ))))
-
 
