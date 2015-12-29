640d55016cc728e6cacbd070766f36c916520000
Thomas Schank <DrTom@schank.ch>
2015-12-29 16:10:27 +0100

    Revert "Remove unreachable groups code"
    This reverts commit a1203b3b7b3114c87ed2a153a5896b2673cfb96c.

---
+++ src/madek/api/resources/groups/index.clj
@@ -0,0 +1,36 @@
+(ns madek.api.resources.groups.index
+  (:require
+    [cider-ci.utils.rdbms :as rdbms]
+    [clj-logging-config.log4j :as logging-config]
+    [clojure.java.jdbc :as jdbc]
+    [clojure.tools.logging :as logging]
+    [compojure.core :as cpj]
+    [logbug.catcher :as catcher]
+    [logbug.debug :as debug]
+    [madek.api.pagination :as pagination]
+    [madek.api.resources.shared :as shared]
+    [honeysql.sql :refer :all]
+    ))
+
+(defn- build-index-base-query []
+  (-> (sql-select :groups.id)
+      (sql-from :groups)))
+
+(defn- build-index-query [{:keys [user-id]}]
+  (cond-> (build-index-base-query)
+    user-id
+    (-> (sql-merge-join :groups_users
+                        [:= :groups.id :groups_users.group_id])
+        (sql-merge-where [:= :groups_users.user_id user-id]))))
+
+(defn get-index [query-params]
+  (let [query (-> (build-index-query query-params)
+                  (pagination/add-offset-for-honeysql query-params)
+                  sql-format)]
+    (logging/info query)
+    (jdbc/query (rdbms/get-ds) query)))
+
+;### Debug ####################################################################
+;(logging-config/set-logger! :level :debug)
+;(logging-config/set-logger! :level :info)
+;(debug/debug-ns *ns*)
