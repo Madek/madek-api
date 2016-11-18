76a5fbdab93470bfe9b8e4951eb2b35a1c235f64
Thomas Schank <DrTom@schank.ch>
2016-11-18 17:58:57 +0100

    Fix: remove failing auth spec which should have failed before WTF

--- spec/resources/media-file/user_authorization_spec.rb
+++ spec/resources/media-file/user_authorization_spec.rb
@@ -30,24 +30,6 @@ describe 'Getting a media-file resource with authentication' do
 
   include_context :auth_media_file_resource_via_json_roa
 
-  context :check_forbidden_without_required_permission do
-    before :example do
-      @media_entry.user_permissions << \
-        FactoryGirl.create(:media_entry_user_permission,
-                           get_full_size: false,
-                           user: @entity)
-      group = FactoryGirl.create(:group)
-      @entity.groups << group
-      @media_entry.group_permissions << \
-        FactoryGirl.create(:media_entry_group_permission,
-                           get_full_size: false,
-                           group: group)
-    end
-    it 'is forbidden 403' do
-      expect(response.status).to be == 403
-    end
-  end
-
   context :check_allowed_if_responsible do
     before :example do
       @media_entry.update_attributes! responsible_user: @entity
