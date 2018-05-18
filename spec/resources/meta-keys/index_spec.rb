6c92eab2c2ebe47d81a2dd0d8b222f5c9b24f8fe
Michał Podlecki <m.podlecki@op.pl>
2018-05-18 11:36:29 +0200

    check user/group permissions when returning vocabularies, meta keys and meta data

--- spec/resources/meta-keys/index_spec.rb
+++ spec/resources/meta-keys/index_spec.rb
@@ -14,5 +14,77 @@ describe 'index' do
       expect(meta_keys_resource.response.status).to be == 200
       expect(meta_keys_resource.data['meta-keys'].count).to be == 0
     end
+
+    context 'when user is authenticated' do
+      context 'when view permission is true' do
+        it 'returns meta key in collection through the user permissions' do
+          vocabulary = FactoryGirl.create(:vocabulary,
+                                          enabled_for_public_view: false)
+          meta_key = FactoryGirl.create(:meta_key,
+                                        id: "#{vocabulary.id}:#{Faker::Lorem.word}",
+                                        vocabulary: vocabulary)
+          Permissions::VocabularyUserPermission.create!(user_id: user.id,
+                                                        view: true,
+                                                        vocabulary: vocabulary)
+
+          data = client.get.relation('meta-keys').get.data['meta-keys'].first
+
+          expect(data).to have_key 'id'
+          expect(data['id']).to eq meta_key.id
+        end
+
+        it 'returns meta key in collection through the group permissions' do
+          vocabulary = FactoryGirl.create(:vocabulary,
+                                          enabled_for_public_view: false)
+          meta_key = FactoryGirl.create(:meta_key,
+                                        id: "#{vocabulary.id}:#{Faker::Lorem.word}",
+                                        vocabulary: vocabulary)
+          group = FactoryGirl.create :group
+          group.users << user
+          Permissions::VocabularyGroupPermission.create!(group_id: group.id,
+                                                         view: true,
+                                                         vocabulary: vocabulary)
+
+          data = client.get.relation('meta-keys').get.data['meta-keys'].first
+
+          expect(data).to have_key 'id'
+          expect(data['id']).to eq meta_key.id
+        end
+      end
+
+      context 'when view permission is false' do
+        it 'does not return meta key through the user permissions' do
+          vocabulary = FactoryGirl.create(:vocabulary,
+                                          enabled_for_public_view: false)
+          meta_key = FactoryGirl.create(:meta_key,
+                                        id: "#{vocabulary.id}:#{Faker::Lorem.word}",
+                                        vocabulary: vocabulary)
+          Permissions::VocabularyUserPermission.create!(user_id: user.id,
+                                                        view: false,
+                                                        vocabulary: vocabulary)
+
+          data = client.get.relation('meta-keys').get.data['meta-keys']
+
+          expect(data.count).to be_zero
+        end
+
+        it 'does not return meta key through the group permissions' do
+          vocabulary = FactoryGirl.create(:vocabulary,
+                                          enabled_for_public_view: false)
+          meta_key = FactoryGirl.create(:meta_key,
+                                        id: "#{vocabulary.id}:#{Faker::Lorem.word}",
+                                        vocabulary: vocabulary)
+          group = FactoryGirl.create :group
+          group.users << user
+          Permissions::VocabularyGroupPermission.create!(group_id: group.id,
+                                                                      view: false,
+                                                                      vocabulary: vocabulary)
+
+          data = client.get.relation('meta-keys').get.data['meta-keys']
+
+          expect(data.count).to be_zero
+        end
+      end
+    end
   end
 end
--- src/madek/api/resources/vocabularies/vocabulary.clj
+++ src/madek/api/resources/vocabularies/vocabulary.clj
@@ -3,6 +3,7 @@
     [madek.api.utils.rdbms :as rdbms :refer [get-ds]]
     [madek.api.utils.sql :as sql]
     [madek.api.resources.shared :refer [remove-internal-keys]]
+    [madek.api.resources.vocabularies.permissions :as permissions]
 
     [clj-logging-config.log4j :as logging-config]
     [clojure.java.jdbc :as jdbc]
@@ -10,15 +11,27 @@
     [logbug.debug :as debug]
     ))
 
+(defn- where-clause
+  [id]
+  (let [vocabuary-ids (permissions/accessible-vocabulary-ids)]
+    (if (empty? vocabuary-ids)
+      [:and
+        [:= :vocabularies.id id]
+        [:= :vocabularies.enabled_for_public_view true]]
+      [:or
+        [:and
+          [:= :vocabularies.id id]
+          [:= :vocabularies.enabled_for_public_view true]]
+        [:in :vocabularies.id vocabuary-ids]])))
+
 (defn build-vocabulary-query [id]
   (-> (sql/select :*)
       (sql/from :vocabularies)
-      (sql/merge-where
-        [:= :vocabularies.id id]
-        [:= :vocabularies.enabled_for_public_view true])
+      (sql/where (where-clause id))
       (sql/format)))
 
 (defn get-vocabulary [request]
+  (permissions/extract-current-user request)
   (let [id (-> request :params :id)
         query (build-vocabulary-query id)]
     (if-let [vocabulary (first (jdbc/query (rdbms/get-ds) query))]
