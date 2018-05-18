69ae7c64ab2e51bc46ab4d4c99897bb90125785c
Michał Podlecki <m.podlecki@op.pl>
2018-05-18 11:36:29 +0200

    check user/group permissions when returning vocabularies, meta keys and meta data

--- spec/resources/vocabularies/index_spec.rb
+++ spec/resources/vocabularies/index_spec.rb
@@ -11,5 +11,65 @@ describe 'index' do
       expect(vocabularies_resource.response.status).to be == 200
       expect(vocabularies_resource.data['vocabularies'].count).to be == 0
     end
+
+    context 'when user is authenticated' do
+      context 'when view permission is true' do
+        it 'returns vocabulary in collection through the user permissions' do
+          vocabulary = FactoryGirl.create(:vocabulary,
+                                          enabled_for_public_view: false)
+          Permissions::VocabularyUserPermission.create!(user_id: user.id,
+                                                                     view: true,
+                                                                     vocabulary: vocabulary)
+
+          data = client.get.relation('vocabularies').get.data['vocabularies'].first
+
+          expect(data).to have_key 'id'
+          expect(data['id']).to eq vocabulary.id
+        end
+
+        it 'returns vocabulary in collection through the group permissions' do
+          vocabulary = FactoryGirl.create(:vocabulary,
+                                          enabled_for_public_view: false)
+          group = FactoryGirl.create :group
+          group.users << user
+          Permissions::VocabularyGroupPermission.create!(group_id: group.id,
+                                                                      view: true,
+                                                                      vocabulary: vocabulary)
+
+          data = client.get.relation('vocabularies').get.data['vocabularies'].first
+
+          expect(data).to have_key 'id'
+          expect(data['id']).to eq vocabulary.id
+        end
+      end
+
+      context 'when view permission is false' do
+        it 'does not return vocabulary through the user permissions' do
+          vocabulary = FactoryGirl.create(:vocabulary,
+                                          enabled_for_public_view: false)
+          Permissions::VocabularyUserPermission.create!(user_id: user.id,
+                                                                     view: false,
+                                                                     vocabulary: vocabulary)
+
+          data = client.get.relation('vocabularies').get.data['vocabularies']
+
+          expect(data.count).to be_zero
+        end
+
+        it 'does not return vocabulary through the group permissions' do
+          vocabulary = FactoryGirl.create(:vocabulary,
+                                          enabled_for_public_view: false)
+          group = FactoryGirl.create :group
+          group.users << user
+          Permissions::VocabularyGroupPermission.create!(group_id: group.id,
+                                                                      view: false,
+                                                                      vocabulary: vocabulary)
+
+          data = client.get.relation('vocabularies').get.data['vocabularies']
+
+          expect(data.count).to be_zero
+        end
+      end
+    end
   end
 end
--- src/madek/api/resources/vocabularies/index.clj
+++ src/madek/api/resources/vocabularies/index.clj
@@ -2,6 +2,7 @@
   (:require
     [madek.api.utils.rdbms :as rdbms :refer [get-ds]]
     [madek.api.utils.sql :as sql]
+    [madek.api.resources.vocabularies.permissions :as permissions]
 
     [clojure.java.jdbc :as jdbc]
     [clj-logging-config.log4j :as logging-config]
@@ -9,16 +10,27 @@
     [logbug.catcher :as catcher]
     [logbug.debug :as debug]))
 
-(def ^:private base-query
+(defn- where-clause
+  []
+  (let [vocabuary-ids (permissions/accessible-vocabulary-ids)]
+    (if (empty? vocabuary-ids)
+      [:= :vocabularies.enabled_for_public_view true]
+      [:or
+        [:= :vocabularies.enabled_for_public_view true]
+        [:in :vocabularies.id vocabuary-ids]])))
+
+(defn- base-query
+  []
   (-> (sql/select :id)
       (sql/from :vocabularies)
-      (sql/merge-where [:= :vocabularies.enabled_for_public_view true])
+      (sql/merge-where (where-clause))
       sql/format))
 
 (defn- query-index-resources [request]
-  (jdbc/query (rdbms/get-ds) base-query))
+  (jdbc/query (rdbms/get-ds) (base-query)))
 
 (defn get-index [request]
+  (permissions/extract-current-user request)
   (catcher/with-logging {}
     {:body
      {:vocabularies
