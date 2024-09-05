9ac7b27f50b1c745dc1d9871febcc2bc70e02b59
uvb <urs.vanbinsbergen@zhdk.ch>
2024-09-05 13:44:58 +0200

    chore: update datalayer (rails 7)

--- spec/config/rails.rb
+++ spec/config/rails.rb
@@ -1,29 +1,23 @@
 require 'rails/all'
-# require 'active_support/all'
 
 module Madek
   class Application < Rails::Application
     config.eager_load = false
 
-    config.paths['config/initializers'] << \
-      Rails.root.join('datalayer', 'initializers')
+    config.paths['config/initializers'] << Rails.root.join('datalayer', 'initializers')
 
     config.autoload_paths += [
       Rails.root.join('datalayer', 'lib'),
+      Rails.root.join('datalayer', 'app', 'models', 'concerns'),
       Rails.root.join('datalayer', 'app', 'models'),
       Rails.root.join('datalayer', 'app', 'lib')
     ]
 
     config.paths['config/database'] = ['spec/config/database.yml']
+
+    Rails.application.config.active_record.legacy_connection_handling = false
+    Rails.autoloaders.main.inflector.inflect("json" => "JSON")
   end
 end
-
-# ENV['DATABASE_URL']="Bogus"
-
-# ActiveSupport::Dependencies.autoload_paths += [
-#  Rails.root.join('datalayer', 'lib'),
-#  Rails.root.join('datalayer', 'app', 'models'),
-#  Rails.root.join('datalayer', 'app', 'lib'),
-# ]
 
 Rails.application.initialize!
