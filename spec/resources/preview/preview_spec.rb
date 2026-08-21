c746267b55d4fa6d66201621e52fe33aec89f151
mradl <manuel.radl@zhdk.ch>
2026-08-21 10:47:08 +0200

    feat: add `x_grand` preview size`

--- spec/resources/preview/preview_spec.rb
+++ spec/resources/preview/preview_spec.rb
@@ -11,6 +11,7 @@ SHAS = { medium: "c3f6a3eae014d0fd1b09feb6031d21d5c342b6671d816009f30724dd666c8f
          large: "4adbe33f15234a0f8d669a2ee0a221a5a0b45bc9e3f9db55fd70984fa8f076a1",
          x_large: "4adbe33f15234a0f8d669a2ee0a221a5a0b45bc9e3f9db55fd70984fa8f076a1",
          grand: "66e1eb76ef8079968ff6a3e7519749be3fbb7b05d54a6f1270727273ccb2a539",
+         x_grand: "4adbe33f15234a0f8d669a2ee0a221a5a0b45bc9e3f9db55fd70984fa8f076a1",
          maximum: "4adbe33f15234a0f8d669a2ee0a221a5a0b45bc9e3f9db55fd70984fa8f076a1" }
 
 describe "Getting a random preview for a specific media-entry" do
